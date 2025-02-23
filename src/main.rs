c1d1c01f1055ff82ffc9d6e60733ee1dc15bec93
legend-of-defi <vitalii.shuba96@gmail.com>
2025-02-23 12:35:29 -0800

    Feat: add

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 use std::env;
 use std::sync::Arc;
 
-use crate::bootstrap::read_all_pairs_v2;
+use crate::bootstrap::{read_all_pairs_v2, read_all_reserves};
 use crate::bot::Bot;
 use crate::config::Config;
 use crate::db_service::PairService;
@@ -12,7 +12,6 @@ use crate::utils::logger::setup_logger;
 use crate::utils::providers::create_http_provider;
 use alloy::primitives::address;
 use clap::{Parser, Subcommand};
-use mev_eth::bootstrap::read_all_reserves;
 
 mod bootstrap;
 mod bot;
@@ -44,10 +43,9 @@ async fn run_default_behavior() -> Result<(), Box<dyn std::error::Error>> {
         env::var("DATABASE_URL").unwrap());
 
     let _provider = create_http_provider().await?;
-    let pairs =
+    let _ =
         read_all_pairs_v2(address!("0x8909Dc15e40173Ff4699343b6eB8132c65e18eC6"), 3000).await;
 
-    println!("pairs: {pairs:?}");
 
     let mut conn = establish_connection()?;
 
@@ -55,20 +53,6 @@ async fn run_default_behavior() -> Result<(), Box<dyn std::error::Error>> {
     let pairs = PairService::read_all_pairs(&mut conn);
 
     println!("\nFound {} pairs", pairs.len());
-
-    for pair in pairs {
-        if let Some((pair, token0, token1)) = PairService::read_pair_with_tokens(&mut conn, pair.id)
-        {
-            println!(
-                "Pair: {} - Token0: {} ({}), Token1: {} ({})",
-                pair.address,
-                token0.symbol.unwrap_or_else(|| "Unknown".to_string()),
-                token0.address,
-                token1.symbol.unwrap_or_else(|| "Unknown".to_string()),
-                token1.address,
-            );
-        }
-    }
 
     println!("Database connected successfully!");
 
@@ -93,8 +77,6 @@ async fn run_default_behavior() -> Result<(), Box<dyn std::error::Error>> {
     // }
 
     let all_reserves = read_all_reserves(3000).await;
-
-    println!("\nFound {all_reserves:?} reserves");
 
     let bot = Arc::new(Bot::new(context));
     start_bot(bot).await;
